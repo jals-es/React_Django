fd54d449cf5b9e59ba7c29ec09e7438471c56050
Juan Antonio Lopez Segui <juan.antonio.lis@gmail.com>
2022-01-27 16:49:25 +0100

    create form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import 'alertifyjs/build/css/alertify.css';
 import './App.css';
 import AuthRoute from "./components/AuthRoute"
 import Spinner from './components/spinner';
+import { Suspense } from 'react'
 export default function App() {
   const Home = React.lazy(() => import("./pages/Home"));
   const Login = React.lazy(() => import("./pages/Auth/login"));
@@ -14,10 +15,10 @@ export default function App() {
   return ( 
     <Router>
       <Routes>
-        <Route path="/" element={<React.Suspense fallback={<Spinner/>}><AuthRoute><Home/></AuthRoute></React.Suspense>}/>
-        <Route path="/login/" element={<React.Suspense fallback={<>...</>}><Login/></React.Suspense>} />
-        <Route path="/register/" element={<React.Suspense fallback={<>...</>}><Register/></React.Suspense>} />
-        <Route path="*" element={<React.Suspense fallback={<>...</>}><Err404/></React.Suspense>}/>
+        <Route path="/" element={<Suspense fallback={<Spinner/>}><AuthRoute><Home/></AuthRoute></Suspense>}/>
+        <Route path="/login/" element={<Suspense fallback={<Spinner/>}><Login/></Suspense>} />
+        <Route path="/register/" element={<Suspense fallback={<Spinner/>}><Register/></Suspense>} />
+        <Route path="*" element={<Suspense fallback={<Spinner/>}><Err404/></Suspense>}/>
       </Routes>
     </Router>
   );
--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,15 +1,71 @@
+
+import { useEffect } from 'react';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as Yup from "yup";
 import './home.css'
 export default function Home() {
-  
+
+    useEffect(() => {
+        register('post')
+    });
+
+    const formSchema = Yup.object().shape({
+        post: Yup.string()
+            .required("* Campo requerido")
+            .max(280, "* Campo demasiado largo")
+    });
+
+    const { register, handleSubmit, formState: { errors }, reset, setValue} = useForm({
+        mode: "onSubmit",
+        resolver: yupResolver(formSchema)
+    });
+
+    function ctChars(e) { 
+        let el = e.target,
+            to = setTimeout(function() {
+                let len = el.textContent.length,
+                    ct = document.getElementById("tweetcounter"),
+                    btn = document.getElementById("postbutton"),
+                    warnAt = 20,
+                    max = ct.getAttribute("data-limit");
+
+                ct.innerHTML = max - len;
+                ct.className = len > max - warnAt ? "warn" : "";
+                btn.disabled = len === 0 || len > max ? true : false;
+                el.value = el.textContent;
+                clearTimeout(to);
+            }, 1);
+    }
+
+    function submitPost(data){
+        console.log(data)
+        reset()
+    }
     return (
         <div id="user-feed h-100">
-            <div className="row">
-               <div className="col-12 bg-danger">q</div>
+            <div className="row row1">
+                <div className="col-12 bg-success"></div>
             </div>
-            <div className="row">
-               <div className="col-lg-3 col-md-6 col-sm-12 bg-info">w</div>
-               <div className="col-lg-6 col-md-6 col-sm-12 bg-primary">v</div>
-               <div className="col-lg-3 col-md-12 col-sm-12 bg-warning">q</div>
+            <div className="row row2">
+                <div className="col-lg-3 col-md-6 col-sm-12 bg-warning">
+
+                </div>
+                <div className="col-lg-6 col-md-6 col-sm-12">
+                    <div className='feedSection'>
+                        <form onSubmit={handleSubmit(submitPost)}>
+                            <div className='textarea' id="tweet" onKeyDown={ctChars} contentEditable="true" placeholder="What’s happening?" onInput={(e) => {setValue('post', e.currentTarget.textContent, { shouldValidate: true });}}/>
+                            <div className="bottom">
+                                <span className="text-danger">{errors.post?.message}</span>
+                                <span id='tweetcounter' data-limit="280" >280</span>
+                                <button id='postbutton' type="submit" tabIndex="0" disabled>
+                                    <span tabIndex="-1">Post</span>
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div className="col-lg-3 col-md-12 col-sm-12 bg-danger"></div>
             </div>
         </div>
     )
