797bc64d3fbd7b796c3711f2f3279280692cd0ab
Juan Antonio Lopez Segui <juan.antonio.lis@gmail.com>
2022-02-12 19:01:35 +0100

    update spinner

--- frontend/src/core/api.service.jsx
+++ frontend/src/core/api.service.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const httpClient = axios.create({
-    baseURL: 'http://192.168.11.118:8000'
+    baseURL: 'http://localhost:8000'
 })
 
 if (localStorage.getItem("token")) {
