80c485a8ee914bd54c1d63572cdc426b64a569bc
Juan Antonio Lopez Segui <juan.antonio.lis@gmail.com>
2022-04-11 18:51:09 +0200

    server

--- frontend/src/core/api.service.jsx
+++ frontend/src/core/api.service.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const httpClient = axios.create({
-    baseURL: 'https://metochat.jals.es:8000'
+    baseURL: 'http://localhost:8000'
 })
 
 if (localStorage.getItem("token")) {
