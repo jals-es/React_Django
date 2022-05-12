996799b354920b5ba2df7a2fb7855c53891f13bd
BuildTools <unconfigured@null.spigotmc.org>
2022-05-12 22:42:47 +0200

    cambios server

--- backend/django_server/django_server/settings.py
+++ backend/django_server/django_server/settings.py
@@ -135,7 +135,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 CORS_ORIGIN_WHITELIST = (
     'http://0.0.0.0:8080',
     'http://127.0.0.1:8080',
-    'https://metochat.jals.es'
+    'https://metochat.jals.es',
+    'http://metochat.jals.es'
 )
 
 
